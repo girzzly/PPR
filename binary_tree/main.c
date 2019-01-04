0c019d92d45cbda1c1e3e9bbd18a99313e610f1c
Dominik Krakau <plutomars_de@yahoo.de>
2019-01-04 18:38:03 +0100

    init binary_tree project.

---
+++ binary_tree/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Dominik
+ *
+ * Created on 4. Januar 2019, 18:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
